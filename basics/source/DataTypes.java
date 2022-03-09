2242c835846233d3fa3177cdd56693614f4b566a
ShashikaUdara <techdudelab@gmail.com>
2022-03-09 16:29:15 +0530

    checkScientificNumbers() created

--- basics/source/DataTypes.java
+++ basics/source/DataTypes.java
@@ -2,7 +2,7 @@ public class DataTypes
 {
 	public boolean checkIntegerTypes()
 	{
-		System.out.println("checkIntegerTypes");
+		System.out.println("checkIntegerTypes()");
 		byte byMark = 89;	// 1 byte
 		short shVolumn = 32000;	// 2 bytes
 		int iNumber = 125;	// 4 bytes
@@ -17,7 +17,7 @@ public class DataTypes
 
 	public boolean checkFloatingPointTypes()
 	{
-		System.out.println("checkFloatingPointTypes");
+		System.out.println("checkFloatingPointTypes()");
 		float fAirPressure = 2.56f; 	// 4 bytes
 		double dPI = 3.14159265359d;	// 8 bites
 		System.out.println("float value: " + fAirPressure);
@@ -28,9 +28,28 @@ public class DataTypes
 
 	public boolean checkOtherTypes()
 	{
-		System.out.println("checkOtherTypes");
+		System.out.println("checkOtherTypes()");
 		boolean bPass = true;
+		char cA = 'A';
+		String sGreating = "Welcome to java practice repo";
+
 		System.out.println("boolean value: " + bPass);
+		System.out.println("char value: " + cA);
+		System.out.println("String value: " + sGreating);
+
+		return true;
+	}
+
+	public boolean checkScientificNumbers()
+	{
+		System.out.println("checkScientificNumbers()");
+		float fPIinto10000 = 3.1415e4f; 	// e stand for the power of 10, 
+										// hence the value before 'e' will powerd by 10^the number after 'e' (here it's 4 = 10000)
+		double dNumberOfStudents = 25E3d;
+		double dHeightOfAStudents = 1.58E2d;
+		System.out.println("Scientific float: " + fPIinto10000);
+		System.out.println("Scientific double: " + dNumberOfStudents);
+		System.out.println("Scientific double: " + dHeightOfAStudents + " cm");
 
 		return true;
 	}
--- basics/source/Main.java
+++ basics/source/Main.java
@@ -3,10 +3,11 @@ public class Main
 	public static void main(String []args)
 	{
 		System.out.println("This is the start");
-		
+
 		DataTypes dt = new DataTypes();
 		dt.checkIntegerTypes();
 		dt.checkFloatingPointTypes();
 		dt.checkOtherTypes();
+		dt.checkScientificNumbers();
 	}
 }
