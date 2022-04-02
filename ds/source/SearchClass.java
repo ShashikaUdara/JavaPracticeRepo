dec4f57395989889ccb7dd0f7ebda794fc5de665
ShashikaUdara <techdudelab@gmail.com>
2022-04-02 23:53:02 +0530

    binarySearch created

--- ds/source/SearchClass.java
+++ ds/source/SearchClass.java
@@ -5,8 +5,22 @@ public class SearchClass
 		System.out.println("Constructor SearchClass");
 	}
 
-	public void binarySearch()
+	public void binarySearch(int[] inputArr, int searchVal)
 	{
-		
+		int midIndex = 0;
+		int midNumber = 0;
+		int leftIndex = 0;
+		int rightIndex = inputArr.length;
+
+		while(leftIndex <= rightIndex)
+		{
+			midIndex = (inputArr.length)/2;
+			midNumber = inputArr[midIndex];
+
+			if(midNumber == searchVal)
+			{
+				return midIndex;
+			}
+		}
 	}
 }
